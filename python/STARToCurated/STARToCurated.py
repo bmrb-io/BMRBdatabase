e8ca36321bc18c427d942ef837d76c37798b201b
Kumaran Baskaran <kbaskaran2@wisc.edu>
2016-02-12 14:38:28 -0600

    pythong script updated

--- python/STARToCurated/STARToCurated.py
+++ python/STARToCurated/STARToCurated.py
@@ -3,8 +3,9 @@ Created on Feb 12, 2016
 
 @author: kumaran
 '''
-import psycopg2
-
+import psycopg2,sys
+sys.path.append('/home/kumaran/git/bmrb/python/Metabolomics/StarParser/parser')
+import bmrb
 class BMRBDB(object):
     '''
     classdocs
@@ -15,11 +16,24 @@ class BMRBDB(object):
         '''
         Constructor
         '''
-        self.conn=psycopg2.connect("dbname=%s user=%s host=%s"%(dbname,user,host))
+        #self.conn=psycopg2.connect("dbname=%s user=%s host=%s"%(dbname,user,host))
+    
+    def getSTARTdata(self,bmrbid):
+        self.starData=bmrb.entry.fromDatabase(bmrbid)
+        print self.starData['entry_information']['_Entry_author']
     
     def close(self):
         self.conn.close()
        
+    
+    def load_country_table(self):
+        cur=self.conn.cursor()
+        dat=open('../../country.txt','r').read().split("\n")[:-1]
+        for country in dat:
+            cmd="insert into \"Country\" values(DEFAULT,\'%s\');"%(country)
+            cur.execute(cmd)
+            self.conn.commit()
+        cur.close()
         
     def list_table(self,table_name):
         self.cur1=self.conn.cursor()
@@ -39,6 +53,8 @@ class BMRBDB(object):
 
 
 if __name__=="__main__":
-    p=BMRBDB('bmrb','web','manta.bmrb.wisc.edu')
-    p.list_table("Entry")
-    p.close()
+    #p=BMRBDB('bmrb','web','manta.bmrb.wisc.edu')
+    p=BMRBDB('bmrb','nmr','localhost')
+    #p.load_country_table()
+    #p.close()
+    p.getSTARTdata(15060)

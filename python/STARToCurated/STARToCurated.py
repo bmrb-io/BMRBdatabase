514df3ead910d165c934fb689a468742e3d5cde4
Kumaran Baskaran <kbaskaran2@wisc.edu>
2016-02-15 11:05:58 -0600

    Entry table load script added

--- python/STARToCurated/STARToCurated.py
+++ python/STARToCurated/STARToCurated.py
@@ -5,7 +5,7 @@ Created on Feb 12, 2016
 '''
 import psycopg2,sys
 sys.path.append('/home/kumaran/git/bmrb/python/Metabolomics/StarParser/parser')
-import bmrb
+import bmrb,re
 class BMRBDB(object):
     '''
     classdocs
@@ -33,6 +33,17 @@ class BMRBDB(object):
         cur1.close()
         return outdat
         
+    def insert_into(self,table,col_val,serial_id):
+        cmd="insert into \"%s\" (\"%s\","%(table,serial_id)
+        for i in col_val[:-1]:
+            cmd="%s \"%s\","%(cmd,i[0])
+        cmd="%s \"%s\") values (DEFAULT,"%(cmd,col_val[-1][0])
+        for i in col_val[:-1]:
+            cmd="%s \'%s\',"%(cmd,i[1])
+        cmd="%s \'%s\');"%(cmd,col_val[-1][1])
+        cur2=self.conn.cursor()
+        cur2.execute(cmd)
+        self.conn.commit()
         
         
     def getSTARTdata(self,bmrbid):
@@ -42,8 +53,33 @@ class BMRBDB(object):
     
     def close(self):
         self.conn.close()
-       
+        
+    def get_dict(self,table):
+        dat=open('../../StarToCuratedMapping.csv','r').read()
+        dict=re.findall('%s,(\S+),(\S+),([\Ss]*)'%(table),dat)
+        out_dict={}
+        for i in dict:
+            if i[1]!='serial':
+                out_dict.update({i[1]:i[0]})
+        return out_dict
     
+    def load_table_Entry(self,bmrbid):
+        dat=open('../../StarToCuratedMapping.csv','r').read()
+        Entry_dict=self.get_dict('Entry')
+        self.starData=bmrb.entry.fromDatabase(bmrbid)
+        cv=[]
+        for i in self.starData['entry_information'].tags:
+            try:
+                #print Entry_dict['_Entry.%s'%(i[0])],i[1]
+                cv.append((Entry_dict['_Entry.%s'%(i[0])],i[1]))
+            except KeyError:
+                pass
+        if len(self.find_ID('Entry', cv, 'DB_Entry_ID'))==0:
+            self.insert_into('Entry', cv, 'DB_Entry_ID')
+        else:
+            print "Data for %s already exists"%(bmrbid)
+            
+            
     def load_table_Country(self):
         cur=self.conn.cursor()
         dat=open('../../country.txt','r').read().split("\n")[:-1]
@@ -77,6 +113,9 @@ class BMRBDB(object):
 if __name__=="__main__":
     #p=BMRBDB('bmrb','web','manta.bmrb.wisc.edu')
     p=BMRBDB('bmrb','nmr','localhost')
-    p.load_table_Country()
+    #p.load_table_Country()
+    
+    
+    p.load_table_Entry('15060')
     p.close()
     #p.getSTARTdata(15060)
